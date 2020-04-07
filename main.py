4b92e64f5341210e340e625377ef8bfeeaf246dd
tubo28 <arsenic28@gmail.com>
2020-04-07 20:01:55 +0900

    init

---
+++ main.py
@@ -0,0 +1,81 @@
+'''
+sample:
+100
+つぼ 100
+木曜午前 100
+つぼ 木曜AM 100
+つぼ 木曜am 100
+つぼ 木曜ａｍ １００
+'''
+
+import oauth2client, gspread, csv, optparse
+from oauth2client.service_account import ServiceAccountCredentials
+
+def parse_cmdargs():
+    parser = optparse.OptionParser()
+    parser.add_option('-s', '--sheetkey', action="store", dest="sheetkey", type="string")
+    parser.add_option('-c', '--credential', action="store", dest="credential", type="string")
+    (opt, _) = parser.parse_args()
+    return opt
+
+def load_testdata(filename):
+    table = None
+    with open(filename, mode='r', newline='', encoding='utf-8') as f:
+        reader = csv.reader(f, delimiter='\t')
+        table = [row for row in reader]
+        # return table # <- ok?
+    return table
+
+def get_worksheet(sheetkey, credential_fileneme):
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(credential_fileneme, scope)
+    gc = gspread.authorize(credentials)
+    wks = gc.open_by_key(sheetkey)
+    worksheets = wks.worksheets()
+    return worksheets[0]
+
+def update(table, user, term, price):
+    '''
+    returns the tuple (updated operation list, original history, new history)
+    '''
+    # find the header row and colmn
+    rows = table # just an alias
+    cols = list(map(list, zip(*rows)))
+    users = next(col for col in cols if 'なまえ' in col) # don't work if there exist the user with the name 'なまえ'
+    terms = next(row for row in rows if '月AM' in row) # same as abeve
+    histbegin = terms.index('買値') # inclusive range
+    histend = histbegin + 13 # exclusive range
+
+    # find target indicies of update
+    if user not in users:
+        raise ValueError('ユーザー {} が見つかりません'.format(user))
+    rowid = users.index(user)
+    if term not in terms:
+        raise ValueError('期間 {} が見つかりません'.format(user))
+    colid = terms.index(term)
+
+    # backup
+    orghist = table[rowid][histbegin:histend]
+
+    oplist = []
+    # update
+    table[rowid][colid] = price
+    newhist = table[rowid][histbegin:histend]
+
+    return (oplist, orghist, newhist)
+
+def main():
+    opt = parse_cmdargs()
+    sheetkey, credential = opt.sheetkey, opt.credential
+    print(sheetkey, credential)
+    prod = False
+    if prod:
+        ws = get_worksheet(sheetkey, credential)
+        table = ws.get_all_values()
+    else:
+        table = load_testdata('./testdata.tsv')
+    oplist, org, new = update(table, 'つぼ', '月AM', 100)
+    print(oplist, org, new)
+
+if __name__ == "__main__":
+    main()
