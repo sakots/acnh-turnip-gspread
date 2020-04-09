b7d06650241fbc5e5f6ef377c53ffaac0e90a69f
tubo28 <arsenic28@gmail.com>
2020-04-10 07:35:05 +0900

    add binding service

---
+++ bind.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+import pymongo
+
+
+class MongoNameIdBindService:
+    """
+    bind id to name. it's injective, not one-to-one
+    """
+    def __init__(self, col: pymongo.collection.Collection):
+        self.collection = col
+
+    def bind(self, id_: int, name: str):
+        doc = {
+            "id": id_,
+            "name": name
+        }
+        self.collection.replace_one({'id': id_}, doc, upsert=True)
+
+    def find_name(self, id_: int) -> Optional[str]:
+        res = self.collection.find_one({'id': id_})
+        if res is None:
+            return None
+        else:
+            return res["name"]
---
+++ test_bind.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+import pymongo_inmemory
+
+from bind import MongoNameIdBindService
+
+
+class TestMongoNameIdBindService(TestCase):
+    def test_bind(self):
+        client = pymongo_inmemory.MongoClient()  # No need to provide host
+        db = client['testdb']
+        collection = db['user_bindings']
+
+        binds = MongoNameIdBindService(collection)
+
+        self.assertEqual(binds.find_name(1), None)
+        binds.bind(1, "a")
+        self.assertEqual(binds.find_name(1), "a")
+        binds.bind(1, "b")
+        self.assertEqual(binds.find_name(1), "b")
