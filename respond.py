dc1b5833f1eacd5b804f9b86595340d41e42c620
tubo28 <arsenic28@gmail.com>
2020-04-19 04:11:00 +0900

    apply black

--- respond.py
+++ respond.py
@@ -17,7 +17,7 @@ class RespondService:
         self.bind_service = bind_service
 
     def respond_to(
-            self, message: discord.Message, request: parse_result.ParseResult
+        self, message: discord.Message, request: parse_result.ParseResult
     ) -> Optional[response.Response]:
         author: discord.Member = message.author
         # FIXME: remove too many if. resolve handler method by type(request).__name__ ?
@@ -31,11 +31,10 @@ class RespondService:
             # TODO: @[kabu] を外部から注入する
             return response.warning(
                 title="カブ価と期間が分かりません",
-                description=
-                "正しく入力されていますか？\n"
+                description="正しく入力されていますか？\n"
                 "- `@[kabu] 100` -> 現在時刻で登録\n"
                 "- `@[kabu] 100 月AM` -> 売値を期間を指定して登録\n"
-                "- `@[kabu] 100 買い` -> 買値登録: "
+                "- `@[kabu] 100 買い` -> 買値登録: ",
             )
         elif isinstance(request, parse_result.BindRequest):
             return self.handle_bind_request(author, request)
@@ -55,7 +54,7 @@ class RespondService:
             return response.warning(description="実装されていません。")
 
     def handle_update_request(
-            self, author: discord.Member, request: parse_result.UpdateRequest
+        self, author: discord.Member, request: parse_result.UpdateRequest
     ) -> response.Response:
         # get position to write on sheet 0
         sheet_index = 0
@@ -64,22 +63,25 @@ class RespondService:
         name = self.bind_service.find_name(author.id)
         if name is None:
             # TODO: スプレッドシートのURLを貼る
-            return response.warning(title="スプレッドシートでの名前が bot に登録されていません",
-                                    description=
-                                    "スプレッドシートに名前を入力してから `@[kabu] im [スプレッドシートでの名前]`"
-                                    " とリプライして登録してください。")
+            return response.warning(
+                title="スプレッドシートでの名前が bot に登録されていません",
+                description="スプレッドシートに名前を入力してから `@[kabu] im [スプレッドシートでの名前]`"
+                " とリプライして登録してください。",
+            )
         position = table_service.find_position(name, request.term)
         if isinstance(position, table.UserNotFound):
             logger.info("user not found on table. user: %s", author)
-            return response.warning(title="スプレッドシートからあなたの名前が見つかりません",
-                                    description="bot に登録された名前 `%s` は正しいですか？" % name
-                                    )
+            return response.warning(
+                title="スプレッドシートからあなたの名前が見つかりません",
+                description="bot に登録された名前 `%s` は正しいですか？" % name,
+            )
         if not isinstance(position, table.Found):
             logger.error(
                 "user not found on table. user: %s, request: %s", author, request
             )
-            return response.error(title="スプレッドシートのどこに書けばいいか分かりません",
-                                  description="開発者は確認してください。")
+            return response.error(
+                title="スプレッドシートのどこに書けばいいか分かりません", description="開発者は確認してください。"
+            )
 
         # try to write
         row, column = position.user_row, position.term_column
@@ -92,8 +94,9 @@ class RespondService:
             )
         except Exception as e:
             logger.error("failed to write to table. error: %s", e, exc_info=True)
-            return response.error(title="スプレッドシートへの書き込みに失敗しました",
-                                  description="開発者は確認してください。")
+            return response.error(
+                title="スプレッドシートへの書き込みに失敗しました", description="開発者は確認してください。"
+            )
 
         logger.info(
             "successfully updated. row: %s, column: %s, %s -> %s",
@@ -116,10 +119,13 @@ class RespondService:
                 ("期間", request.term),
                 ("名前", "`%s`" % name),
                 ("履歴", format_history(history)),
-                ("予測", prediction_link(history))]
+                ("予測", prediction_link(history)),
+            ],
         )
 
-    def handle_history_request(self, author: discord.Member) -> Optional[response.Response]:
+    def handle_history_request(
+        self, author: discord.Member
+    ) -> Optional[response.Response]:
         # FIXME: dup
         sheet_index = 0
         raw_table = self.gspread_service.get_table(sheet_index)
@@ -127,48 +133,60 @@ class RespondService:
         name = self.bind_service.find_name(author.id)
         if name is None:
             # FIXME: dup
-            return response.warning(title="スプレッドシートでの名前が bot に登録されていません",
-                                    description=
-                                    "スプレッドシートに名前を入力してから `@[kabu] im [スプレッドシートでの名前]`"
-                                    " とリプライして登録してください。")
+            return response.warning(
+                title="スプレッドシートでの名前が bot に登録されていません",
+                description="スプレッドシートに名前を入力してから `@[kabu] im [スプレッドシートでの名前]`"
+                " とリプライして登録してください。",
+            )
         history = table_service.find_user_history(name)
         if history is None:
-            return response.warning(title="スプレッドシートからあなたの名前が見つかりません",
-                                    description="bot に登録された名前 `%s` は正しいですか？" % name)
-        return response.success(title="あなたの履歴です",
-                                fields=[("履歴", format_history(history)),
-                                        ("予測", prediction_link(history))])
+            return response.warning(
+                title="スプレッドシートからあなたの名前が見つかりません",
+                description="bot に登録された名前 `%s` は正しいですか？" % name,
+            )
+        return response.success(
+            title="あなたの履歴です",
+            fields=[("履歴", format_history(history)), ("予測", prediction_link(history))],
+        )
 
     def handle_bind_request(
-            self, author: discord.Member, request: parse_result.BindRequest
+        self, author: discord.Member, request: parse_result.BindRequest
     ) -> Optional[response.Response]:
         try:
             self.bind_service.bind(author.id, request.name)
         except Exception as e:
             logger.error("failed to bind user. error: %s", e, exc_info=True)
-            return response.error(title="スプレッドシートでの名前を覚える際にエラーが発生しました",
-                                  description="開発者は確認してください。")
+            return response.error(
+                title="スプレッドシートでの名前を覚える際にエラーが発生しました", description="開発者は確認してください。"
+            )
         logger.info("successfully bound. %s is %s, ", author, request.name)
         author_str = author.nick or str(author)
-        return response.success(title="覚えました",
-                                description="%s はスプレッドシートで `%s`。" % (author_str, request.name))
+        return response.success(
+            title="覚えました",
+            description="%s はスプレッドシートで `%s`。" % (author_str, request.name),
+        )
 
-    def handle_who_am_i_request(self, author: discord.Member) -> Optional[response.Response]:
+    def handle_who_am_i_request(
+        self, author: discord.Member
+    ) -> Optional[response.Response]:
         name = self.bind_service.find_name(author.id)
         author_str = author.nick or str(author)
         if name is not None:
             logger.info("successfully found name. author: %s, name: %s", author, name)
-            return response.success(title="覚えてます",
-                                    description="bot に登録された %s のスプレッドシートでの名前は %s です。" % (author_str, name))
+            return response.success(
+                title="覚えてます",
+                description="bot に登録された %s のスプレッドシートでの名前は %s です。" % (author_str, name),
+            )
         else:
             logger.info(
                 "successfully found name but binding not found. author: %s", author
             )
             # FIXME: dup
-            return response.warning(title="スプレッドシートでの名前が bot に登録されていません",
-                                    description=
-                                    "スプレッドシートに名前を入力してから `@[kabu] im [スプレッドシートでの名前]`"
-                                    " とリプライして登録してください。")
+            return response.warning(
+                title="スプレッドシートでの名前が bot に登録されていません",
+                description="スプレッドシートに名前を入力してから `@[kabu] im [スプレッドシートでの名前]`"
+                " とリプライして登録してください。",
+            )
 
 
 def format_history(history: List[str]) -> str:
